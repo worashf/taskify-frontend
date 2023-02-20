3e6a99fbb9e7801bbe0977064a095ccd6379262f
Worash <uppert83@gmail.com>
2023-02-20 16:00:47 +0300

    Create project structure

---
+++ src/components/layout/MetaData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MetaData = () => {
+  return (
+    <div>MetaData</div>
+  )
+}
+
+export default MetaData
---
+++ src/utils/axiosConfig.js
@@ -0,0 +1,8 @@
+ import axios from "axios";
+const baseUrl = `http://localhost:6000/${api / v1}`;
+
+
+const axiosInstance = axios.create({
+    baseURL,
+})
+export default axiosInstance;
