64393ee515fbb91c6317706d5501c84488a1042b
Worash <uppert83@gmail.com>
2023-04-21 16:04:41 +0300

    Add footer social component

---
+++ src/components/footer/SocialCard.js
@@ -0,0 +1,13 @@
+
+import {LinkedIn} from "@mui/icons-material"
+import {Box, }  from "@mui/material"
+
+const SocialCard = () => {
+  return (
+      <Box>
+          
+     </Box>
+  )
+}
+
+export default SocialCard
