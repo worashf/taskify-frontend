668429b80bf2b4a813c0cfa39fcc0ea27054d664
Worash <uppert83@gmail.com>
2023-04-21 16:05:12 +0300

    Add  layout

---
+++ src/layout/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+ import {Box} from "@mui/material"
+const Footer = () => {
+  return (
+    <Box sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height:"100px"
+ 
+      
+    }}>
+    
+   </Box>
+  )
+}
+
+export default Footer
