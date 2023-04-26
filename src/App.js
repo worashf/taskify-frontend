a97bfb2a201d96e0972dad2df93430d5dfce6d16
Worash <uppert83@gmail.com>
2023-04-26 17:38:15 +0300

    Add theme, and context provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import { useMode, ColorModeContext } from "./theme";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import SideBar from "./layout/Sidebar";
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <h1> Taskify </h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SideBar />
+          <main className="content">
+            <Header />
+            <Routes></Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
