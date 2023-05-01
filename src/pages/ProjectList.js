5b43e6717667e965e6bb1a8e6db85a1e17fd4547
worashf <uppert83@gmail.com>
2023-05-01 22:38:05 +0300

    Create project card layout

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -1,8 +1,37 @@
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  Grid,
+  useTheme,
+  Divider,
+} from "@mui/material";
 import { tokens } from "../theme";
 import PageHeader from "../components/page-header/PageHeader";
-import { AddCircleOutlineOutlined } from "@mui/icons-material";
+import {
+  AddCircleOutlineOutlined,
+  CheckCircleOutlineOutlined,
+  DonutLargeOutlined,
+} from "@mui/icons-material";
 
+import ProjectItem from "../components/project/ProjectItem";
+
+const projects = [
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "finished" },
+  { name: "Create app", status: "finished" },
+  { name: "Create app", status: "finished" },
+  { name: "Create app", status: "finished" },
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "open" },
+  { name: "Create app", status: "open" },
+];
+
+const openedProjects = projects.filter((item) => item.status === "open");
+const finishedProjects = projects.filter((item) => item.status === "finished");
 const ProjectList = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -29,7 +58,7 @@ const ProjectList = () => {
         <Button
           variant="outined"
           sx={{
-            backgroundColor: colors.greenAccent[500],
+            backgroundColor: colors.greenAccent[700],
             p: 1,
             "&:hover": {
               backgroundColor: colors.primary[400],
@@ -40,9 +69,86 @@ const ProjectList = () => {
           Add new project
         </Button>
       </Box>
-      <Box>
-        <Typography> Projects list</Typography>
-      </Box>
+
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <DonutLargeOutlined
+              sx={{ color: colors.greenAccent[700], fontSize: "30px" }}
+            />
+
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                fontStyle: "italic",
+                color: colors.blueAccent[400],
+              }}
+            >
+              Opened
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              mt: 2,
+              display: "grid",
+              gridTemplateColumns: { md: "1fr 1fr" },
+              gap: 2,
+            }}
+          >
+            {openedProjects.map((item) => (
+              <ProjectItem name={item.name} status={item.status} />
+            ))}
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <CheckCircleOutlineOutlined
+              sx={{ color: colors.greenAccent[700], fontSize: "30px" }}
+            />
+
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                fontStyle: "italic",
+                color: colors.blueAccent[400],
+              }}
+            >
+              Finished
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              mt: 2,
+              display: "grid",
+              gridTemplateColumns: { md: "1fr 1fr" },
+              gap: 2,
+            }}
+          >
+            {finishedProjects.map((item) => (
+              <ProjectItem name={item.name} status={item.status} />
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
