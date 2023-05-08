b37457949fd9b0befc5b2fe824925e4116572a44
Worash <uppert83@gmail.com>
2023-05-08 17:16:17 +0300

    Update axios instance

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
- import axios from "axios";
-const baseUrl = `http://localhost:6000/${api / v1}`;
-
+import axios from "axios";
+const baseURL = `http://localhost:6000/${api / v1}`;
 
 const axiosInstance = axios.create({
-    baseURL,
-})
+  baseURL,
+  headers: { "Content-Type": "application/json" },
+});
 export default axiosInstance;
